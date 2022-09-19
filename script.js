aa8a61059c6af781bed4540c385c24ca01a49b5a
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2022-09-19 17:26:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// const one = document.getElementById('1').textContent;
+// const two = document.getElementById('2').textContent;
+// const three = document.getElementById('3').textContent;
+// const four = document.getElementById('4').textContent;
+// const five = document.getElementById('5').textContent;
+// const six = document.getElementById('6').textContent;
+// const seven = document.getElementById('7').textContent;
+// const eight = document.getElementById('8').textContent;
+// const nine = document.getElementById('9').textContent;
+// const zero = document.getElementById('0').textContent;
+
+let number = document.querySelectorAll('.number').textContent;
+console.log(number);
